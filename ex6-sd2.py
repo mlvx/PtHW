31beee97f6562b803fa3275ab7647ce79b5f8d27
Mike Malveaux <mike.malveaux@gmail.com>
2019-03-18 02:22:51 -0700

    LPtHW ex6-sd2: Adding SD3, SD4.
    Exercise 6, Study Drills 3 and 4, don't seem worth putting into
    their own separate files.
     - SD2:  Identify the strings-within-strings.
     - SD3:  Justify the IDs in SD2.
       - Decimal (%d) and Boolean are not strings.
       - Concatenation is not a string-within-string.
     - SD4:  Explain concatenation of two strings.

--- ex6-sd2.py
+++ ex6-sd2.py
@@ -18,11 +18,26 @@ w = "This is the left side of..."
 e = "a string with a right side."
 
 print w + e 
+print w , e # Testing diff '+' vs ','
 
 # Exercise 6 Study Drills
 
 # SD1: Write an explanatory comment above each line.
-# SD2: Find all four places where a string is put inside a(nother) string.
-# Lines 4 (two instances), 9, and 10.  Lines 13 and 15 don't count -- 
-# "hilarious" is a Boolean, not a string.  Line 20 doesn't count -- it's
-# a concatenation of two strings, not putting one string inside another.
+
+print """
+SD2: Find all four places where a string is put inside a[nother] string.
+Line 4 (two instances): y = "Those who know %s and those who %s." % (binary, do_not)
+Line 9: print "I said: %r." % x
+Line 10: print "I also said: '%s'." % y
+"""
+
+print """
+SD3: Are you sure there are only four places?
+Lines 13 and 15 don't count --  "hilarious" is a Boolean, not a string.
+Line 20 doesn't count -- it's a concatenation of two strings, not
+putting one string inside another.
+"""
+
+print """
+SD4: Explain why adding the two strings /w/ and /e/ with /+/ makes a longer string.
+"""
