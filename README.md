7557de85d406e589b86ca183f65399ac735229d1
Mike Malveaux <mike.malveaux@gmail.com>
2019-03-18 01:49:01 -0700

    LPtHW, ex6, sd1: variables, formats, comments.

--- README.md
+++ README.md
@@ -9,4 +9,6 @@ The classic by Zed Shaw.  There will be code!
 2019-03-10T14:29-07:00  Working on LPtHW, exercise 4, intro to variables.  Using Vim's "ctrl-r" autocomplete features to get var names right -- so nice.
 2019-03-10T17:22-07:00  Done for today.  Interesting -- choosing whether variables are integer or floating-point can have repercussions later.
 2019-03-16T04:23-0700	Forgot to timestamp when I started a couple hours ago.  Did ex5-sd3 -- all the format characters.  There actually are several I'd forgotten about, like %u and %U for Unicode, %x and %X for hexadecimal, etc.  Next:  Do ex5-sd4, converting Shaw's measurements into metric.
+2019-03-18T01:46-07:00	Earlier, did ex6; forgot to update README.  Hm; given the existence of Git Log, perhaps README is not the proper place to track progress.  I may revamp this.
+2019-03-18T01:47-07:00	Currently working on ex6 study drills.  I dislike adding comments to each line, but I do perceive the educational value -- forces me to actually think about some of the lines.
 
---
+++ ex6-sd1.py
@@ -0,0 +1,41 @@
+# Defines var 'x' as text string, with embedded "decimal" format for external number.
+x = "There are %d types of people." % 10
+# Defines var 'binary' as text string.
+binary = "binary"
+# Defines var 'do_not' as text string.
+do_not = "don't"
+# Defines var 'y' as text string, with embedded "string" format for 2 external numbers.
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+# Prints the "There are 10 types of people" variable.
+print x
+# Prints the "Those who know binary..." variable.
+print y
+
+# Prints a text string, with the "There are 10 types..." string var, formatted
+#    with "raw/rep" converter (surrounds with single quotation marks).
+print "I said: %r." % x
+# Prints a text string, with the "Those who know binary..." string var, formatted
+#    with "string" converter.  This doesn't surround with quotation marks, which
+#    in this case are included in the text string of the print statement ('%s').
+print "I also said: '%s'." % y
+
+# Defines 'hilarious' as a Boolean var with a value of "False".
+hilarious = False
+# Defines 'joke_evaluation' as a text string, that formats an arbitrary var with 
+#    the "raw/rep" converter.
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+# Prints "Isn't that joke..." string var, calling the Boolean "False" var formatted
+#    with "raw/rep" converter.
+print joke_evaluation % hilarious
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print w + e 
+
+# Exercise 6 Study Drills
+
+# SD1: Write an explanatory comment above each line.
+# Ugh.  Really?  Ok.
