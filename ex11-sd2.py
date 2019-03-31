a90eddc5d36c9c9b8eb6738c65f7ed0c8d23d0b1
Mike Malveaux <mike.malveaux@gmail.com>
2019-03-30 22:46:56 -0700

    PtHW ex11-sd2: Other ways to use raw_input.

--- ex11-sd2.py
+++ ex11-sd2.py
@@ -9,4 +9,12 @@ print "So, you're %r old, %r tall and %r heavy." % (
     age, height, weight)
 
 # Study Drills
+# SD2:  Find and try out other ways to use "raw_input".
+# The most obvious way is with a prompt.
+
+print
+fav_num = raw_input("What's your favorite number? ")
+fav_num = float(fav_num)
+print "The square of your favorite number is", fav_num * fav_num
+
 
