8573027a952dcac5d73a7c6a8bebb0d197ccce10
Mike Malveaux <mike.malveaux@gmail.com>
2019-04-16 00:46:30 -0700

    PtHW ex13-sd2a: argv practice.

--- ex13-sd2a.py
+++ ex13-sd2a.py
@@ -1,11 +1,19 @@
-from sys import argv
-
-script, first, second, third = argv
-
-print "The script is called:", script
-print "Your first variable is:", first
-print "Your second variable is:", second 
-print "Your third variable is:", third 
+# from sys import argv
+# 
+# script, first, second, third = argv
+# 
+# print "The script is called:", script
+# print "Your first variable is:", first
+# print "Your second variable is:", second 
+# print "Your third variable is:", third 
 
 # Study Drills
-# SD2, part A:
+# SD2, part A:	Write a script that has fewer arguments (than the orginal script).
+
+from sys import argv
+
+scriptname, var_one, var_two = argv
+
+print "Your script is named %s." % scriptname
+print "The first argument was", var_one + "."
+print "The last argument was %r." % var_two
