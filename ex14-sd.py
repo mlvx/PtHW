4079bbd10ba2993879286fa2bc94b4a617cca471
Mike Malveaux <mike.malveaux@gmail.com>
2019-04-21 00:56:45 -0700

    PtHW ex14-sd: practice argv; game Adventure.

--- ex14-sd.py
+++ ex14-sd.py
@@ -1,9 +1,10 @@
 from sys import argv
 
-script, user_name = argv
-prompt = '> '
+script, user_name, clan = argv
+prompt = 'Type-Input-Here:_'
 
-print "Hi %s, I'm the %s script." % (user_name, script)
+print "Hi %s of Clan %s, I'm the %s script." % (
+    user_name, clan, script)
 print "I'd like to ask you a few questions."
 print "Do you like me %s?" % user_name
 likes = raw_input(prompt)
@@ -11,14 +12,17 @@ likes = raw_input(prompt)
 print "Where do you live %s?" % user_name
 lives = raw_input(prompt)
 
+print "Where does Clan %s come from?" % clan
+origin = raw_input(prompt)
+
 print "What kind of computer do you have?"
 computer = raw_input(prompt)
 
 print """
 Alright, so you said %r about liking me.
-You live in %r.  Not sure where that is.
+You live in %r, and Clan %r is from %r.  Not sure where that is.
 And you have a %r computer.  Nice.
-""" % (likes, lives, computer)
+""" % (likes, lives, clan, origin, computer) 
 
 # Study Drills
 
@@ -34,3 +38,20 @@ And you have a %r computer.  Nice.
 # running on a PDP-10.  Apparently, Zork was published commercially in three
 # parts; and was considered an especially rich example of the genre, with a
 # sophisticated text parser.
+# Windows Subsystem for Linux includes a version of Adventure.  Started
+# playing.  A dwarf threw an axe at me.  I got 54 points out of possible 350.
+# If / when I resume, it's "adven_2019-0421".
+
+# SD2:  Change the  prompt  variable to something else entirely.
+# Done.  Trivial; but it's nice to change in one place, and have it work
+# everywhere.  The power of variables.
+
+# SD3:  Add another argument and use it in your script.
+# Done.
+
+# SD4:  Make sure you understand combining """ multi-line strings with the %
+# format 'activator' as the last print.
+# That's actually kind of slick.  The  %  operator and the opening paren have
+# to be on the same line as the closing quotemark; but if the line is getting
+# long, then you can put the arguments on the next line.  Indentation is
+# optional, but good style for readability.
