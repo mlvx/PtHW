94a9643871cdba5bb208ca178a198054c1206022
Mike Malveaux <mike.malveaux@gmail.com>
2019-04-23 23:59:50 -0700

    PtHW ex15 sd4: tidy.

--- ex15-sd3.py
+++ ex15-sd3.py
@@ -22,15 +22,3 @@ print txt_again.read()
 #       "functions" and "methods".  Search online to see what other people
 #       do to define these.  Do not fret if it is confusing -- it is normal
 #       for programmers to confuse you with vast extensive knowledge.
-# SD4:  Get rid of the part from lines 10-15 where you use  raw_input  and
-#       try the script without them.
-# SD5:  Use only  raw_input  and try the script that way.  Think of why one
-#       way of getting the filename would be better than another.
-# SD6:  Run  pydoc file  and scroll down until you see the  read()  command
-#       (method / function).  See all the other ones you can use?  Skip the
-#       ones that have  __  (two underscores) in front because those are
-#       junk.  [?]  Try some of the other commands (methods / functions).
-# SD7:  Start python again and use  open  from the prompt.  Notice how you
-#       can open files and run  read  on them right there?
-# SD8:  Have your script also do a  close()  on the  txt  and  txt_again
-#       variables.  It's important to close files when done with them.
