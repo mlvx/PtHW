52241e0e195baf7c17f934fe68a7493edc145dc9
Mike Malveaux <mike.malveaux@gmail.com>
2019-04-24 01:27:49 -0700

    PtHW ex15-sd5: functions, methods
    Study Drill 5 has us using functions / methods, and checking
    results when only using raw_input.

--- ex15-sd5.py
+++ ex15-sd5.py
@@ -1,13 +1,13 @@
-from sys import argv
+# from sys import argv
+# 
+# script, filename = argv
+# 
+# txt = open(filename)
+# 
+# print "Here's your file %r:" % filename
+# print txt.read()
 
-script, filename = argv
-
-txt = open(filename)
-
-print "Here's your file %r:" % filename
-print txt.read()
-
-print "Type the filename again:"
+print "Type the filename:"   # Don't need 'again' in this context.
 file_again = raw_input("> ")
 
 txt_again = open(file_again)
@@ -15,23 +15,21 @@ txt_again = open(file_again)
 print txt_again.read()
 
 # Study Drills
-# SD1:  Above each line, write out in English what it does.
-# SD2:  If you are not sure, ask someone for help or search online.  Often,
-#       searching for "python FOO" will find answer for what that FOO does
-#       in Python.  Try searching for "python open".
-# SD3:  We've used the label "commands", but these are usually called 
-#       "functions" and "methods".  Search online to see what other people
-#       do to define these.  Do not fret if it is confusing -- it is normal
-#       for programmers to confuse you with vast extensive knowledge.
-# SD4:  Get rid of the part from lines 10-15 where you use  raw_input  and
-#       try the script without them.
-# SD5:  Use only  raw_input  and try the script that way.  Think of why one
-#       way of getting the filename would be better than another.
-# SD6:  Run  pydoc file  and scroll down until you see the  read()  command
-#       (method / function).  See all the other ones you can use?  Skip the
-#       ones that have  __  (two underscores) in front because those are
-#       junk.  [?]  Try some of the other commands (methods / functions).
-# SD7:  Start python again and use  open  from the prompt.  Notice how you
-#       can open files and run  read  on them right there?
-# SD8:  Have your script also do a  close()  on the  txt  and  txt_again
-#       variables.  It's important to close files when done with them.
+# SD1 (comment each line), SD2 (research online documentation):
+# See file ex15-sd12.py.
+# SD3:  "Commands", "functions", "methods":  Do some research.
+# Done; work in progress.  See file ex15-sd3.py.
+# SD4:  Get rid of the part from lines 10-15 where you use  raw_input  ...
+# Done (commented out).  The script runs fine, but uses only the variables
+# populated with argv when first running the script.
+#
+print """
+SD5:  Use only  raw_input  and try the script that way.  Think of why one
+      way of getting the filename would be better than another.
+
+The script runs fine using only  raw_input  and not  argv.
+Two ways of getting the filename:  argv and raw_input.
+
+argv may be less prone to user error (depending on how the script is
+launched).  argv also has less flexibility than raw_input.  On the other
+hand,  raw_input  has flexibility for handling unexpected complications.
